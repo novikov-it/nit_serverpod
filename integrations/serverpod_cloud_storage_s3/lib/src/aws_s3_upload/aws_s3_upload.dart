aa708fdc3ea43fe07132166f54e72b0091769a7e
Strelkovik <135411036+Strelkovik@users.noreply.github.com>
2025-06-02 14:59:25 +0200

    Add content type to policy

--- integrations/serverpod_cloud_storage_s3/lib/src/aws_s3_upload/aws_s3_upload.dart
+++ integrations/serverpod_cloud_storage_s3/lib/src/aws_s3_upload/aws_s3_upload.dart
@@ -6,6 +6,7 @@ import 'dart:typed_data';
 
 import 'package:amazon_cognito_identity_dart_2/sig_v4.dart';
 import 'package:http/http.dart' as http;
+import 'package:http_parser/http_parser.dart' as http_parser;
 import 'package:path/path.dart' as path;
 
 import 'policy.dart';
@@ -77,44 +78,33 @@ class AwsS3Uploader {
 
   /// Upload a file, returning the file's public URL on success.
   static Future<String?> uploadData({
-    /// AWS access key
     required String accessKey,
-
-    /// AWS secret key
     required String secretKey,
-
-    /// The name of the S3 storage bucket to upload  to
     required String bucket,
     required String endpoint,
-
-    /// The file to upload
     required ByteData data,
-
-    /// The path to upload the file to (e.g. "uploads/public"). Defaults to the root "directory"
     String destDir = '',
-
-    /// The AWS region. Must be formatted correctly, e.g. us-west-1
     required String region,
-
-    /// The filename to upload as. If null, defaults to the given file's current filename.
     required String uploadDst,
     bool public = true,
   }) async {
-    // final uploadDest = '$destDir/${filename ?? path.basename(file.path)}';
-
     final stream = http.ByteStream.fromBytes(data.buffer.asUint8List());
-
-    // final stream = http.ByteStream(Stream.castFrom(file.openRead()));
     final length = data.lengthInBytes;
 
     final uri = Uri.parse(endpoint);
     final req = http.MultipartRequest("POST", uri);
-    final multipartFile = http.MultipartFile('file', stream, length,
-        filename: path.basename(uploadDst));
 
     final policy = Policy.fromS3PresignedPost(
         uploadDst, bucket, accessKey, 15, length,
         region: region, public: public);
+    final multipartFile = http.MultipartFile(
+      'file',
+      stream,
+      length,
+      filename: path.basename(uploadDst),
+      contentType: http_parser.MediaType.parse(policy.contentType!),
+    );
+
     final key =
         SigV4.calculateSigningKey(secretKey, policy.datetime, region, 's3');
     final signature = SigV4.calculateSignature(key, policy.encode());
@@ -122,6 +112,7 @@ class AwsS3Uploader {
     req.files.add(multipartFile);
     req.fields['key'] = policy.key;
     req.fields['acl'] = public ? 'public-read' : 'private';
+    req.fields['Content-Type'] = policy.contentType!;
     req.fields['X-Amz-Credential'] = policy.credential;
     req.fields['X-Amz-Algorithm'] = 'AWS4-HMAC-SHA256';
     req.fields['X-Amz-Date'] = policy.datetime;
@@ -132,8 +123,9 @@ class AwsS3Uploader {
       final res = await req.send();
 
       if (res.statusCode >= 400 && res.statusCode < 500) {
-        stderr.writeln(
-            'Failed to upload to AWS, with reason: ${res.reasonPhrase}');
+        final responseBody = await res.stream.bytesToString();
+        stderr.writeln('Failed to upload to AWS: ${res.reasonPhrase}');
+        stderr.writeln('Response: $responseBody');
       }
 
       if (res.statusCode == 204) return '$endpoint/$uploadDst';
@@ -197,6 +189,7 @@ class AwsS3Uploader {
         'X-Amz-Date': policy.datetime,
         'Policy': policy.encode(),
         'X-Amz-Signature': signature,
+        'Content-Type': policy.contentType ?? 'application/octet-stream',
       },
     };
 
--- integrations/serverpod_cloud_storage_s3/lib/src/aws_s3_upload/policy.dart
+++ integrations/serverpod_cloud_storage_s3/lib/src/aws_s3_upload/policy.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
 
 import 'package:amazon_cognito_identity_dart_2/sig_v4.dart';
+import 'package:mime/mime.dart';
+import 'package:path/path.dart' as path;
 
 class Policy {
   String expiration;
@@ -11,10 +13,11 @@ class Policy {
   String datetime;
   int maxFileSize;
   bool public;
+  String? contentType;
 
   Policy(this.key, this.bucket, this.datetime, this.expiration, this.credential,
       this.maxFileSize,
-      {this.region = 'us-east-1', this.public = true});
+      {this.region = 'us-east-1', this.public = true, this.contentType});
 
   factory Policy.fromS3PresignedPost(
     String key,
@@ -24,6 +27,7 @@ class Policy {
     int maxFileSize, {
     String region = 'us-east-1',
     bool public = true,
+    String? contentType,
   }) {
     final datetime = SigV4.generateDatetime();
     final expiration = (DateTime.now())
@@ -34,9 +38,10 @@ class Policy {
         .join('T');
     final cred =
         '$accessKeyId/${SigV4.buildCredentialScope(datetime, region, 's3')}';
+    contentType ??= _getContentType(key);
 
     return Policy(key, bucket, datetime, expiration, cred, maxFileSize,
-        region: region, public: public);
+        region: region, public: public, contentType: contentType);
   }
 
   String encode() {
@@ -46,18 +51,53 @@ class Policy {
 
   @override
   String toString() {
+    final conditions = [
+      '{"bucket": "$bucket"}',
+      '["starts-with", "\$key", "$key"]',
+      '{"acl": "${public ? 'public-read' : 'private'}"}',
+      '["content-length-range", 1, $maxFileSize]',
+      '{"x-amz-credential": "$credential"}',
+      '{"x-amz-algorithm": "AWS4-HMAC-SHA256"}',
+      '{"x-amz-date": "$datetime"}',
+    ];
+
+    // Добавляем условие для content-type если оно указано
+    if (contentType != null) {
+      conditions.add('["starts-with", "\$Content-Type", "$contentType"]');
+    }
+
     return '''
 { "expiration": "$expiration",
   "conditions": [
-    {"bucket": "$bucket"},
-    ["starts-with", "\$key", "$key"],
-    {"acl": "${public ? 'public-read' : 'private'}"},
-    ["content-length-range", 1, $maxFileSize],
-    {"x-amz-credential": "$credential"},
-    {"x-amz-algorithm": "AWS4-HMAC-SHA256"},
-    {"x-amz-date": "$datetime" }
+    ${conditions.join(',\n    ')}
   ]
 }
 ''';
+  }
+}
+
+// Добавьте вспомогательную функцию для определения content-type
+String _getContentType(String filename) {
+  final mimeType = lookupMimeType(filename);
+  if (mimeType != null) {
+    return mimeType;
+  }
+
+  final extension = path.extension(filename).toLowerCase();
+
+  switch (extension) {
+    case '.mp3':
+      return 'audio/mpeg';
+    case '.wav':
+      return 'audio/wav';
+    case '.jpg':
+    case '.jpeg':
+      return 'image/jpeg';
+    case '.png':
+      return 'image/png';
+    case '.pdf':
+      return 'application/pdf';
+    default:
+      return 'application/octet-stream';
   }
 }
